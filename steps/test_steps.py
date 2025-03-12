a2c628cf4685bf43df9d7a5671f3dcec5759c1e4
Saravana Kumar L <saravana.l@hcltech.com>
2025-03-12 23:06:45 +0530

    Initial commit

---
+++ steps/test_steps.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+
+@given('User is on the login page')
+def open_login_page(context):
+    context.driver = webdriver.Chrome()
+    context.driver.get("https://www.saucedemo.com/")
+
+@when('User enters valid username and password')
+def enter_valid_credentials(context):
+    username_input = context.driver.find_element(By.ID, 'user-name')
+    username_input.send_keys('standard_user')
+    password_input = context.driver.find_element(By.ID, 'password')
+    password_input.send_keys('secret_sauce')
+
+@when('User enters invalid username and password')
+def enter_invalid_credentials(context):
+    username_input = context.driver.find_element(By.ID, 'user-name')
+    username_input.send_keys('invalid_username')
+    password_input = context.driver.find_element(By.ID, 'password')
+    password_input.send_keys('invalid_password')
+
+@when('User clicks on the login button')
+def click_login_button(context):
+    login_button = context.driver.find_element(By.ID, 'login-button')
+    login_button.click()
+
+@then('User should be redirected to the home page')
+def verify_redirect_to_home_page(context):
+    assert "https://www.saucedemo.com/inventory.html" in context.driver.current_url
+
+@then('Page title should contain "Swag Labs"')
+def verify_page_title(context):
+    assert "Swag Labs" in context.driver.title
+
+@then('Error message should start with "Epic sadface"')
+def verify_error_message(context):
+    error_message = context.driver.find_element(By.CSS_SELECTOR, 'h3[data-test="error"]')
+    assert error_message.text.startswith("Epic sadface")
